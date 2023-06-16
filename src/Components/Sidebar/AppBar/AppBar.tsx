93666f3343200981ad85bdc6224eb5aba8490f3f
Cory Rowlee <rowlee.development@gmail.com>
2023-06-16 01:09:21 -0500

    removed unneeded code

--- src/Components/Sidebar/AppBar/AppBar.tsx
+++ src/Components/Sidebar/AppBar/AppBar.tsx
@@ -1,25 +1,18 @@
-import * as React from "react";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import Switch from "@mui/material/Switch";
 
-
-
-export default function ThemeAppBar({check, change}) {
-    
-
+export default function ThemeAppBar({ check, change }) {
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="fixed" color="default" className="AppBar"
-      >
+      <AppBar position="fixed" color="default" className="AppBar">
         <Toolbar>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             Todo List
           </Typography>
-          <Switch color="default" onChange={change}
-          checked={check}/>
+          <Switch color="default" onChange={change} checked={check} />
         </Toolbar>
       </AppBar>
     </Box>
