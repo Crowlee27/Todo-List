aa365be4de27d0d63ccbecc725a14f6d5fe78499
Cory Rowlee <rowlee.development@gmail.com>
2023-06-16 01:10:47 -0500

    removed unneeded code

--- src/Components/Sidebar/Profile/ProfileDialog.tsx
+++ src/Components/Sidebar/Profile/ProfileDialog.tsx
@@ -1,24 +1,29 @@
-import React from 'react';
 import { useState } from "react";
-import { Dialog, DialogTitle, DialogContent, DialogActions, Typography,Button, TextField } from "@mui/material";
-import TaskForm from '../TaskForm/TaskForm';
-import TaskDescription from '../TaskForm/TaskDescription';
-import {DialogProps} from '@mui/material/Dialog';
+import {
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+  Button,
+} from "@mui/material";
+import TaskForm from "../TaskForm/TaskForm";
 
-
+import { DialogProps } from "@mui/material/Dialog";
 
 export default function ProfileDialog({ open, handleClose }) {
-    const [fullWidth] = useState(true);
-    const [maxWidth] = useState<DialogProps['maxWidth']>('lg');
-  
-    return (
-    <Dialog open={open} onClose={handleClose} fullWidth={fullWidth} maxWidth={maxWidth} >
+  const [fullWidth] = useState(true);
+  const [maxWidth] = useState<DialogProps["maxWidth"]>("lg");
+
+  return (
+    <Dialog
+      open={open}
+      onClose={handleClose}
+      fullWidth={fullWidth}
+      maxWidth={maxWidth}
+    >
       <DialogTitle>New Task</DialogTitle>
       <DialogContent>
-      <TaskForm/>
-      {/* <TaskDescription/> */}
-       
-        {/* Additional content and input fields can be added here */}
+        <TaskForm />
       </DialogContent>
       <DialogActions>
         <Button onClick={handleClose}>Submit</Button>
