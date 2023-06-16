0b708e79cbda612037e5532c416b7abc2deeaf05
Cory Rowlee <rowlee.development@gmail.com>
2023-06-16 01:13:16 -0500

    finished TaskSelect

--- src/Components/Sidebar/TaskForm/TaskSelect.tsx
+++ src/Components/Sidebar/TaskForm/TaskSelect.tsx
@@ -3,14 +3,31 @@ import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select, { SelectChangeEvent } from '@mui/material/Select';
 
-export default function TaskSelect() {
+export default function TaskSelect(props: ISelectField) {
+const {
+    name= "StatusBox",
+    label= "StatusBox",
+    value= "",
+    items= [{value:"", label:"Add Items"}],
+    onChange= (e: SelectChangeEvent)=> console.log(e),
+}= props;
+
     return (
         <FormControl fullWidth variant='standard' size='small'>
-            <InputLabel id='statusLabel'>Status</InputLabel>
-            <Select>
-                <MenuItem >To Do 1</MenuItem>
-                <MenuItem >To Do 2</MenuItem>
-                <MenuItem >To Do 3</MenuItem>
+            <InputLabel id={`${name}-id`}>{label}</InputLabel>
+            <Select
+            labelId={`${name}-id`}
+            id={`${name}-id-select`}
+            value={value}
+            label={label}
+            name={name}
+            onChange={onChange}
+            >
+                {items.map((item, index)=> (
+                    <MenuItem key={index} value={item.value}>{item.label}</MenuItem>
+                ))}
+                )
+                
             </Select>
         </FormControl>
     )
