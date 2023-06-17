069d4864b3e615589004732463aeb8fe4b044f99
Cory Rowlee <rowlee.development@gmail.com>
2023-06-16 22:03:01 -0500

    add global interfaces

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,4 +1,5 @@
-import { SelectChangeEvent } from '@mui/material/Select';
+import { SelectChangeEvent } from "@mui/material/Select";
+import { Status } from "./src/Components/Sidebar/TaskFormEnum/Status";
 export {};
 
 declare global {
@@ -7,23 +8,51 @@ declare global {
     onChange?: (date: Date | null) => void;
   }
 
- interface ITextField {
+  interface ITextField {
     id: string;
     name: string;
     label: string;
- }
+  }
 
- interface ISelectItems {
+  interface ISelectItems {
     value: string;
     label: string;
-}
+  }
 
-interface ISelectField {
+  interface ISelectField {
     name: string;
     label: string;
     value: string;
     items: ISelectItems[];
-    onChange?: (e: SelectChangeEvent)=> void;
+    onChange?: (e: SelectChangeEvent) => void;
+  }
 
-}
+  type TaskCounterStatusType = Status.todo | Status.inProgress | Status.done;
+
+  interface ITaskCounter {
+    count?: number;
+    status?: TaskCounterStatusType;
+  }
+  interface ITaskHeader {
+    title?: string;
+    date?: Date;
+  }
+
+  interface ITaskFooter {
+    onStatusChange?: (e: React.ChangeEvent<HTMLSelectElement>) => void;
+    onClick?: (
+      e:
+        | React.MouseEvent<HTMLButtonElement>
+        | React.MouseEvent<HTMLAnchorElement>
+    ) => void;
+  }
+  interface ITaskDescription {
+    description?: string;
+  }
+
+  interface ITask extends ITaskHeader, ITaskDescription, ITaskFooter {
+    id?: string;
+    priority?: string;
+    status?: string;
+  }
 }
