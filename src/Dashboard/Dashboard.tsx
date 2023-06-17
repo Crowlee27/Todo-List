6252f3e3165879e651f20fcd7bb26e2aee384a9b
Cory Rowlee <rowlee.development@gmail.com>
2023-06-16 22:10:19 -0500

    cleaned up code

--- src/Dashboard/Dashboard.tsx
+++ src/Dashboard/Dashboard.tsx
@@ -1,17 +1,13 @@
-import {Grid} from "@mui/material";
-import ToDoComponent from "../features/todo/ToDoComponent";
+import { Grid } from "@mui/material";
+
 import Sidebar from "../Components/Sidebar/SideBar";
 import TaskArea from "../Components/TaskArea/TaskArea";
-import {CssBaseline} from "@mui/material";
-
 
 export default function Dashboard() {
-    return (
-        <Grid container minHeight="100vh" p={0} m={0}>
-           
-            <Sidebar/>
-            <TaskArea/>
-           
-        </Grid>
-    )
+  return (
+    <Grid container minHeight="100vh" p={0} m={0}>
+      <Sidebar />
+      <TaskArea />
+    </Grid>
+  );
 }
