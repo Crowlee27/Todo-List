b9744cba6f58603d46586783af3396bec3047350
Cory Rowlee <rowlee.development@gmail.com>
2023-06-16 22:07:10 -0500

    complete task form

--- src/Components/Sidebar/TaskForm/TaskForm.tsx
+++ src/Components/Sidebar/TaskForm/TaskForm.tsx
@@ -12,30 +12,45 @@ export default function TaskForm() {
     <Box>
       <Stack>
         <Stack marginBottom="25px">
-          <TaskTitle id={"Task Title"} name={"Task Title"} label={"Task Title"}  />
-          <TaskDescription id={"Description"} name={"Description"} label={"Description"} />
+          <TaskTitle
+            id={"Task Title"}
+            name={"Task Title"}
+            label={"Task Title"}
+          />
+          <TaskDescription
+            id={"Description"}
+            name={"Description"}
+            label={"Description"}
+          />
         </Stack>
 
         <TaskDate />
         <Stack direction="row" spacing={5}>
-          <TaskSelect name={"Status"} label={"Status"} value={""} items={[
-            {value: Status.todo,
-            label: Status.todo},
-            {
-              value: Status.inProgress,
-              label: Status.inProgress
-            }
-          ]} />
-          <TaskSelect name={"Priority"} label={"Priority"} value={""} items={[
-            {value: Priority.low,
-              label: Priority.low},
+          <TaskSelect
+            name={"Status"}
+            label={"Status"}
+            value={""}
+            items={[
+              { value: Status.todo, label: Status.todo },
               {
-                value:Priority.normal,
-                label: Priority.normal
+                value: Status.inProgress,
+                label: Status.inProgress,
               },
-              {value: Priority.high,
-                label: Priority.high}
-          ]} />
+            ]}
+          />
+          <TaskSelect
+            name={"Priority"}
+            label={"Priority"}
+            value={""}
+            items={[
+              { value: Priority.low, label: Priority.low },
+              {
+                value: Priority.normal,
+                label: Priority.normal,
+              },
+              { value: Priority.high, label: Priority.high },
+            ]}
+          />
         </Stack>
       </Stack>
     </Box>
