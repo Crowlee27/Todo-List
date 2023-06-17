6aca43c73aabdb88d78de52315a4f1bc62ef6072
Cory Rowlee <rowlee.development@gmail.com>
2023-06-16 22:05:55 -0500

    format

--- src/Components/Sidebar/TaskForm/TaskDate.tsx
+++ src/Components/Sidebar/TaskForm/TaskDate.tsx
@@ -2,16 +2,16 @@ import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { DateTimePicker } from "@mui/x-date-pickers/DateTimePicker";
 
-
-export default function TaskDate(props: ITaskDate)  {
-// const {
-//     onChange = (date) => console.log(date),
-// } = props;
+export default function TaskDate(props: ITaskDate) {
+  // const {
+  //     onChange = (date) => console.log(date),
+  // } = props;
 
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
-      <DateTimePicker label="Due Date"
-       {...props}
+      <DateTimePicker
+        label="Due Date"
+        {...props}
         // onChange={onChange}
       />
     </LocalizationProvider>
