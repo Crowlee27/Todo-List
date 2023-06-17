b42298378d2a67eb06b7745c2a29c75b7d0b73ca
Cory Rowlee <rowlee.development@gmail.com>
2023-06-16 22:05:20 -0500

    fixed background issue

--- src/Components/Sidebar/Sidebar.tsx
+++ src/Components/Sidebar/Sidebar.tsx
@@ -3,22 +3,23 @@ import { Grid } from "@mui/material";
 
 export default function Sidebar() {
   return (
-    <Grid item md={1}
-        sx={{
-            backgroundColor: "background.paper",
-            height: "100vh",
-            position: "fixed",
-            left: 0,
-            top: 64,
-            width: "100%",
-            display: "flex",
-            justifyContent: "flex-start",
-            flexDirection: "column",
-            alignItems: "center",
-        }}
+    <Grid
+      item
+      md={1}
+      sx={{
+        backgroundColor: "background.paper",
+        height: "100vh",
+        position: "fixed",
+        left: 1,
+        top: 64,
+        width: "100%",
+        display: "flex",
+        justifyContent: "flex-start",
+        flexDirection: "column",
+        alignItems: "center",
+      }}
     >
-       <Profile />
-       
+      <Profile />
     </Grid>
   );
 }
