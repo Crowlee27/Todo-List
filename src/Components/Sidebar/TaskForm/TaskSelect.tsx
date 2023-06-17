036041fa8068b9baf1c59face97b29c91c756f41
Cory Rowlee <rowlee.development@gmail.com>
2023-06-16 22:07:28 -0500

    format

--- src/Components/Sidebar/TaskForm/TaskSelect.tsx
+++ src/Components/Sidebar/TaskForm/TaskSelect.tsx
@@ -1,34 +1,35 @@
-import InputLabel from '@mui/material/InputLabel';
-import MenuItem from '@mui/material/MenuItem';
-import FormControl from '@mui/material/FormControl';
-import Select, { SelectChangeEvent } from '@mui/material/Select';
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select, { SelectChangeEvent } from "@mui/material/Select";
 
 export default function TaskSelect(props: ISelectField) {
-const {
-    name= "StatusBox",
-    label= "StatusBox",
-    value= "",
-    items= [{value:"", label:"Add Items"}],
-    onChange= (e: SelectChangeEvent)=> console.log(e),
-}= props;
+  const {
+    name = "StatusBox",
+    label = "StatusBox",
+    value = "",
+    items = [{ value: "", label: "Add Items" }],
+    onChange = (e: SelectChangeEvent) => console.log(e),
+  } = props;
 
-    return (
-        <FormControl fullWidth variant='standard' size='small'>
-            <InputLabel id={`${name}-id`}>{label}</InputLabel>
-            <Select
-            labelId={`${name}-id`}
-            id={`${name}-id-select`}
-            value={value}
-            label={label}
-            name={name}
-            onChange={onChange}
-            >
-                {items.map((item, index)=> (
-                    <MenuItem key={index} value={item.value}>{item.label}</MenuItem>
-                ))}
-                )
-                
-            </Select>
-        </FormControl>
-    )
+  return (
+    <FormControl fullWidth variant="standard" size="small">
+      <InputLabel id={`${name}-id`}>{label}</InputLabel>
+      <Select
+        labelId={`${name}-id`}
+        id={`${name}-id-select`}
+        value={value}
+        label={label}
+        name={name}
+        onChange={onChange}
+      >
+        {items.map((item, index) => (
+          <MenuItem key={index} value={item.value}>
+            {item.label}
+          </MenuItem>
+        ))}
+        )
+      </Select>
+    </FormControl>
+  );
 }
